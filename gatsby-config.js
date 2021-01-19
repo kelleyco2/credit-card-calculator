ece18ca7fa1fe4c4e41ff318bf6fe943f0eb73d1
Cooper Kelley <kelleyco2@gmail.com>
2021-01-18 22:02:50 -0700

    Adds gatsby-plugin-root-import

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+const path = require("path");
+
 module.exports = {
   siteMetadata: {
     title: "credit-card-calculator",
@@ -10,6 +12,15 @@ module.exports = {
       options: {
         accessToken: `${process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN}`,
         spaceId: `${process.env.GATSBY_CONTENTFUL_SPACE_ID}`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-root-import",
+      options: {
+        root: path.join(__dirname, "src"),
+        images: path.join(__dirname, "src/images"),
+        components: path.join(__dirname, "src/components"),
+        utils: path.join(__dirname, "src/utils"),
       },
     },
     "gatsby-plugin-styled-components",
