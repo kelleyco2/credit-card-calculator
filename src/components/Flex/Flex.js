07b5dfc36eaf706865619b74137060e9b212b7b5
Cooper Kelley <kelleyco2@gmail.com>
2021-01-21 15:34:13 -0700

    Add flex component

---
+++ src/components/Flex/Flex.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Flex = styled.div`
+  display: flex;
+  flex-direction: ${({ column }) => (column ? "column" : "row")};
+  justify-content: ${({ justify }) => (justify ? justify : "space-between")};
+  align-items: ${({ align }) => (align ? align : "center")};
+  flex-wrap: ${({ wrap }) => (wrap ? "wrap" : "nowrap")};
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  padding: ${({ padding }) => padding};
+  margin: ${({ margin }) => margin};
+`;
+
+export default Flex;
